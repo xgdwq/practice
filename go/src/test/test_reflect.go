a6ba803f116bd3c39e3098207c6b4facef0f0cf2
yuzhiwang <yuzhiwang@tencent.com>
2019-10-07 12:06:29 +0800

    detail reflect

--- go/src/test/test_reflect.go
+++ go/src/test/test_reflect.go
@@ -12,6 +12,11 @@ func main() {
 	fmt.Println("type:", v.Type())
 	fmt.Println("kind is float64:", v.Kind() == reflect.Float64)
 	fmt.Println("value:", v.Float())
+    /*
+	type: float64
+    kind is float64: true
+    value: 3.4
+	*/
 
 	p := reflect.ValueOf(&x)
 	v1 := p.Elem()
