6cfcb34995cf07f7f52dfde3acf6b09bef98baf5
yuzhiwang <yuzhiwang@tencent.com>
2020-01-30 21:47:54 +0800

    add test err

---
+++ go/src/test/test_err.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"errors"
+)
+
+var ErrDivByZero error = errors.New("division by zero")
+
+func div(x, y int) (int, error) {
+	if y == 0 {
+		return 0, ErrDivByZero
+	}
+	return x / y, nil
+}
+
+func main() {
+	switch z, err := div(10, 0); err {
+	case nil:
+		println(z)
+	case ErrDivByZero:
+		panic(err)
+	}
+}
